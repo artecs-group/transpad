d9bd9658aacad23335c3909dccb10e014535849f
Tommaso Marinelli <tommarin@ucm.es>
2023-04-26 22:20:02 +0200

    Update compiler script

--- transpad.py
+++ transpad.py
@@ -1,24 +1,24 @@
+#!/usr/bin/env python3
 import siliconcompiler                            # import python package
 
 def main():
-    chip = siliconcompiler.Chip()                 # create chip object
-    chip.set('source', ['register.v',
-                        'counter.v',
-                        'mux4g.v',
-                        'decoder4.v',
-                        'decoder8.v',
-                        'transpad_dp.v',
-                        'transpad_cu.v',
-                        'transpad.v'])
-    chip.set('design', 'transpad')                # set top module
-    chip.set('constraint', 'transpad.sdc')        # set constraints file
-    chip.set('pdk', 'wafersize', 300)
+    chip = siliconcompiler.Chip('transpad')       # create chip object
+    chip.input('register.v')	                  # set input files
+    chip.input('counter.v')
+    chip.input('mux4g.v')
+    chip.input('decoder4.v')
+    chip.input('decoder8.v')
+    chip.input('transpad_dp.v')
+    chip.input('transpad_cu.v')
+    chip.input('transpad.v')
+    chip.clock('clk', period=1.0)                 # set clock period
     chip.load_target('freepdk45_demo')            # load predefined target
     chip.write_flowgraph("transpad.png")
     chip.write_manifest("transpad.tcl")
+    chip.set('option', 'nodisplay', True)
+    chip.set('option', 'novercheck', True)
     chip.run()                                    # run compilation
     chip.summary()                                # print results summary
-    chip.show()                                   # show layout file
 
 if __name__ == '__main__':
     main()
